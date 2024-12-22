e4f4dd4c5afa419ca849bfc188960a8165e2bc51
Keita ATTICOT <keita.atticot@ledger.fr>
2024-12-22 21:58:22 +0100

    fix(tests): fix test file
    main

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -14,6 +14,20 @@ async function detectFlashLoanRequest(
   });
 }
 
+// flashloan on block 16818057
+const flashloanTransaction = [
+  {
+    blockNumber: 16818057,
+    txHash:
+      "0x71a908be0bef6174bccc3d493becdfd28395d78898e355d451cb52f7bac38617",
+    initiator: "0x036cec1a199234fC02f72d29e596a09440825f1C",
+    target: "0x036cec1a199234fC02f72d29e596a09440825f1C",
+    asset: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+    amount: "300000000000",
+    premium: "270000000",
+  },
+];
+
 describe("Flash Loan Detection API", () => {
   // let server: any;
 
@@ -32,7 +46,18 @@ describe("Flash Loan Detection API", () => {
     const data = await response.json();
     expect(data).toEqual({
       blockNumber: 12345,
-      hasFlashLoan: true,
+      hasFlashLoan: [false, []],
+    });
+  });
+
+  it("should detect flashloan transactions valid request", async () => {
+    const response = await detectFlashLoanRequest(16818057);
+    expect(response.status).toBe(200);
+
+    const data = await response.json();
+    expect(data).toEqual({
+      blockNumber: 16818057,
+      hasFlashLoan: [true, flashloanTransaction],
     });
   });
 
