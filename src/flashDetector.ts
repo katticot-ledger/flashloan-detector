ea33d91b24de49f44a1ed5fe1f2aa65b3bb9fa80
Keita ATTICOT <keita.atticot@ledger.fr>
2024-12-22 21:14:39 +0100

    feat: update api with flashloan detector updated

--- src/flashDetector.ts
+++ src/flashDetector.ts
@@ -1,3 +1,10 @@
-export async function detectFlashLoan(blockNumber: number): Promise<boolean> {
-  return true;
+import { fetchFlashLoanTransactions } from "./blockReader.ts";
+import { FlashLoanTransaction } from "./types.ts";
+
+export async function detectFlashLoan(
+  blockNumber: number,
+): Promise<[boolean, FlashLoanTransaction[]]> {
+  const transactions = await fetchFlashLoanTransactions(blockNumber);
+
+  return [transactions.length > 0, transactions];
 }
